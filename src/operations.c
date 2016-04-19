f478d71d77ff076878b68eb8ae0d73c7d1899c31
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-04-19 14:05:11 +0200

    Change rewind system

--- src/operations.c
+++ src/operations.c
@@ -6,11 +6,34 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/14 15:40:38 by tguillem          #+#    #+#             */
-/*   Updated: 2016/04/19 13:43:21 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/04/19 14:04:26 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	rewind_stack(t_stack *stack, int index)
+{
+	int	distance;
+
+	distance = stack->a_size - 1 - index;
+	if (index < distance && index != 0)
+	{
+		while (index < stack->a_size)
+		{
+			print_op(stack, "ra");
+			++index;
+		}
+	}
+	else
+	{
+		while (index)
+		{
+			print_op(stack, "rra");
+			--index;
+		}
+	}
+}
 
 void		resolve_pushswap(t_stack *stack)
 {
@@ -26,16 +49,12 @@ void		resolve_pushswap(t_stack *stack)
 			stack->a[index] = stack->a[index + 1];
 			stack->a[index + 1] = tmp;
 			print_op(stack, "sa");
-			while (index < stack->a_size)
-			{
-				print_op(stack, "ra");
-				index++;
-			}
+			rewind_stack(stack, index);
 			index = -1;
 		}
 		else
 			print_op(stack, "ra");
-		index++;
+		++index;
 	}
 	print_op(stack, "ra");
 }
