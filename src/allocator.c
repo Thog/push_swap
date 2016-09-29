1d50df50c07a2beb2b4ae727903029a353c03dbd
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-09-29 10:40:59 +0200

    Normify

--- src/allocator.c
+++ src/allocator.c
@@ -6,7 +6,7 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 16:57:04 by tguillem          #+#    #+#             */
-/*   Updated: 2016/09/21 16:57:04 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/09/29 10:34:49 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int				fill_stack(t_nlist *a, int ac, char **av)
 	return (1);
 }
 
-t_node		*new_node(int data)
+t_node			*new_node(int data)
 {
 	t_node		*res;
 
@@ -58,7 +58,7 @@ t_node		*new_node(int data)
 	return (res);
 }
 
-void		destroy_nlist(t_nlist **lst)
+void			destroy_nlist(t_nlist **lst)
 {
 	t_node		*node;
 	t_node		*tmp;
--- src/checker.c
+++ src/checker.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/29 10:33:38 by tguillem          #+#    #+#             */
+/*   Updated: 2016/09/29 10:40:10 by tguillem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void 			show_stacks(t_nlist *a, t_nlist *b)
+void			show_stacks(t_nlist *a, t_nlist *b)
 {
 	t_node	*node;
 
@@ -22,40 +34,44 @@ void 			show_stacks(t_nlist *a, t_nlist *b)
 	ft_putstr_fd("\n", 2);
 }
 
+static int		apply_operation(t_nlist *a, t_nlist *b, char *buffer)
+{
+	if (!ft_strcmp("sa", buffer))
+		sa(a);
+	else if (!ft_strcmp("sb", buffer))
+		sb(b);
+	else if (!ft_strcmp("ss", buffer))
+		ss(a, b);
+	else if (!ft_strcmp("pa", buffer))
+		pa(a, b);
+	else if (!ft_strcmp("pb", buffer))
+		pb(a, b);
+	else if (!ft_strcmp("ra", buffer))
+		ra(a);
+	else if (!ft_strcmp("rb", buffer))
+		rb(b);
+	else if (!ft_strcmp("rr", buffer))
+		rr(a, b);
+	else if (!ft_strcmp("rra", buffer))
+		rra(a);
+	else if (!ft_strcmp("rrb", buffer))
+		rrb(b);
+	else if (!ft_strcmp("rrr", buffer))
+		rrr(a, b);
+	else
+		return (ft_error_retint("Error\n", 0));
+	return (1);
+}
+
 void			apply_checker(t_nlist *a, t_nlist *b)
 {
-	char	*buffer; // FIXME: LEAKING
+	char	*buffer;
 
 	show_stacks(a, b);
 	while (get_next_line(0, &buffer) > 0)
 	{
-		if (!ft_strcmp("sa", buffer))
-			sa(a);
-		else if (!ft_strcmp("sb", buffer))
-			sb(b);
-		else if (!ft_strcmp("ss", buffer))
-			ss(a, b);
-		else if (!ft_strcmp("pa", buffer))
-			pa(a, b);
-		else if (!ft_strcmp("pb", buffer))
-			pb(a, b);
-		else if (!ft_strcmp("ra", buffer))
-			ra(a);
-		else if (!ft_strcmp("rb", buffer))
-			rb(b);
-		else if (!ft_strcmp("rr", buffer))
-			rr(a, b);
-		else if (!ft_strcmp("rra", buffer))
-			rra(a);
-		else if (!ft_strcmp("rrb", buffer))
-			rrb(b);
-		else if (!ft_strcmp("rrr", buffer))
-			rrr(a, b);
-		else
-		{
-			ft_putstr_fd("Error\n", 2);
+		if (!apply_operation(a, b, buffer))
 			return ;
-		}
 		show_stacks(a, b);
 	}
 	ft_printf("%s\n", (is_already_sorted(a) && !b->start) ? "OK" : "KO");
